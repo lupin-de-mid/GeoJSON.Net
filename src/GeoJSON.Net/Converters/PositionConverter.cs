123f3053964fce50fa3e44c61a05a4204d14a8be
Lupin Demid <lupin@demid.su>
2012-11-05 19:48:14 +0400

    Some  implementation of PositionConverter

--- src/GeoJSON.Net/Converters/PositionConverter.cs
+++ src/GeoJSON.Net/Converters/PositionConverter.cs
@@ -7,6 +7,9 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System.Globalization;
+using System.Text;
+
 namespace GeoJSON.Net.Converters
 {
     using System;
@@ -29,7 +32,21 @@ namespace GeoJSON.Net.Converters
         /// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter"/> to write to.</param><param name="value">The value.</param><param name="serializer">The calling serializer.</param>
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            throw new NotImplementedException();
+			writer.WriteStartArray();
+			var g = value as List<IPosition>;
+	        var sb = new StringBuilder();
+	        foreach (var position in g)
+	        {
+				
+		        var geographicPosition = position as GeographicPosition;
+		        sb.Append(string.Format("[{0},{1}],", geographicPosition.Latitude.ToString(CultureInfo.InvariantCulture), geographicPosition.Longitude.ToString(CultureInfo.InvariantCulture)));
+				
+	        }
+	        var s = sb.ToString();
+	        var startIndex = s.Length-1;
+	        s= s.Remove(startIndex);
+			writer.WriteRawValue(s);
+	        writer.WriteEndArray();
         }
 
         /// <summary>
